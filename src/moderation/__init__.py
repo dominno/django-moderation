9aef37e8474d62f75868f6eebb36cc82f9e4f614
Dominik Szopa <dszopa@gmail.com>
2010-03-16 19:36:18 +0100

    Added GenericModerator class that encapsulates moderation options for a given model. Chnaged register method, it  will get only two parameters: model class and settings class. Added option to register models with multiple manager.

--- src/moderation/__init__.py
+++ src/moderation/__init__.py
@@ -16,55 +16,86 @@ class RegistrationError(Exception):
     """Exception thrown when registration with Moderation goes wrong."""
 
 
-class ModerationInfo(object):
-    """Stores registration information about a model."""
-
-    def __init__(self, model_class, manager_name, moderation_manager_class,
-                 moderated_object_name, base_manager,
-                 notification_class, 
-                 automoderate_in_admin=False):
-        """Initializes the moderation info."""
+class GenericModerator(object):
+    """
+    Encapsulates moderation options for a given model.
+    """
+    manager_names = ['objects']
+    moderation_manager_class = ModerationObjectsManager
+    
+    auto_approve_for_staff = True
+    auto_approve_for_groups = None
+    auto_reject_for_groups = None
+    
+    notify_moderator = True
+    notify_user = True
+    
+    subject_template_moderator\
+     = 'moderation/notification_subject_moderator.txt'
+    message_template_moderator\
+     = 'moderation/notification_message_moderator.txt'
+    
+    subject_template_user = 'moderation/notification_subject_user.txt'
+    message_template_user = 'moderation/notification_message_user.txt'
+    
+    def __init__(self, model_class):
         self.model_class = model_class
-        self.manager_name = manager_name
-        self.moderation_manager_class = moderation_manager_class
-        self.moderated_object_name = moderated_object_name
-        self.base_manager = base_manager
-        self.automoderate_in_admin = automoderate_in_admin
-        self.notification_class = notification_class
+        self.base_managers = self._get_base_managers()
+        
+    def send(self, content_object, subject_template, message_template,
+                           recipient_list, extra_context=None):
+        context = {
+                'moderated_object': content_object.moderated_object,
+                'content_object': content_object,
+                'site': Site.objects.get_current(),
+                'content_type': content_object.moderated_object.content_type}
 
+        if extra_context:
+            context.update(extra_context)
 
-class ModerationManager(object):
+        message = render_to_string(message_template, context)
+        subject = render_to_string(subject_template, context)
 
-    def __init__(self):
-        """Initializes the moderation manager."""
+        send_mail(subject=subject,
+                  message=message,
+                  from_email=settings.DEFAULT_FROM_EMAIL,
+                  recipient_list=recipient_list)
 
-        self._registered_models = {}
+    def inform_moderator(self,
+            content_object,
+            extra_context=None):
+        '''Send notification to moderator'''
+        from moderation.conf.settings import MODERATORS
+        if self.notify_moderator:
+            self.send(content_object=content_object,
+                  subject_template=self.subject_template_moderator,
+                  message_template=self.message_template_moderator,
+                  recipient_list=MODERATORS)
 
-    def register(self, model_class,
-                 manager_name='objects',
-                 moderation_manager_class=ModerationObjectsManager,
-                 moderated_object_name='moderated_object',
-                 notification_class=BaseModerationNotification):
-        """Registers model class with moderation"""
-        if model_class in self._registered_models:
-            msg = "%s has been registered with Moderation." % model_class
-            raise RegistrationError(msg)
-        base_manager = self._get_base_manager(model_class, manager_name)
-
-        moderation_info = ModerationInfo(model_class,
-                                        manager_name,
-                                        moderation_manager_class,
-                                        moderated_object_name,
-                                        base_manager,
-                                        notification_class,
-                                        )
-        self._registered_models[model_class] = moderation_info
-        self._and_fields_to_model_class(model_class,
-                                        base_manager,
-                                        manager_name,
-                                        moderation_manager_class,
-                                        moderated_object_name)
-        self._connect_signals(model_class)
+    def inform_user(self, content_object,
+                user,
+                extra_context=None):
+        '''Send notification to user when object is approved or rejected'''
+        if extra_context:
+            extra_context.update({'user': user})
+        else:
+            extra_context = {'user': user}
+        if self.notify_user:
+            self.send(content_object=content_object,
+                  subject_template=self.subject_template_user,
+                  message_template=self.message_template_user,
+                  recipient_list=[user.email],
+                   extra_context=extra_context)
+    
+    def _get_base_managers(self):
+        base_managers = []
+        
+        for manager_name in self.manager_names:
+            base_managers.append(
+                        (manager_name,
+                        self._get_base_manager(self.model_class,
+                                               manager_name)))
+        return base_managers
     
     def _get_base_manager(self, model_class, manager_name):
         """Returns base manager class for given model class """
@@ -75,57 +106,93 @@ class ModerationManager(object):
 
         return base_manager
 
+
+class ModerationManager(object):
+
+    def __init__(self):
+        """Initializes the moderation manager."""
+        self._registered_models = {}
+
+    def register(self, model_class, moderator_class=None):
+        """Registers model class with moderation"""
+        if model_class in self._registered_models:
+            msg = u"%s has been registered with Moderation." % model_class
+            raise RegistrationError(msg)
+        if not moderator_class:
+            moderator_class = GenericModerator
+
+        if not issubclass(moderator_class, GenericModerator):
+            msg = 'moderator_class must subclass '\
+                  'GenericModerator class, found %s' % moderator_class
+            raise AttributeError(msg)
+
+        self._registered_models[model_class] = moderator_class(model_class)
+
+        self._and_fields_to_model_class(self._registered_models[model_class])
+        self._connect_signals(model_class)
+
     def _connect_signals(self, model_class):
         from django.db.models import signals
         signals.pre_save.connect(self.pre_save_handler,
                                      sender=model_class)
         signals.post_save.connect(self.post_save_handler,
                                       sender=model_class)
+    
+    def _add_moderated_object_to_class(self, model_class):
+        relation_object = generic.GenericRelation(ModeratedObject,
+                                               object_id_field='object_pk')
+        
+        model_class.add_to_class('_moderated_object', relation_object)
 
-    def _and_fields_to_model_class(
-                            self, 
-                            model_class,
-                            base_manager,
-                            manager_name='objects',
-                            moderation_manager_class=ModerationObjectsManager,
-                            moderated_object_name='moderated_object'):
+        def get_modarated_object(self):
+            return getattr(self, '_moderated_object').get()
+
+        model_class.add_to_class('moderated_object',
+                                 property(get_modarated_object))
+
+    def _and_fields_to_model_class(self, moderator_class_instance):
         """Sets moderation manager on model class,
            adds generic relation to ModeratedObject,
            sets _default_manager on model class as instance of
            ModerationObjectsManager
         """
-        ModerationObjectsManager = moderation_manager_class()(base_manager)
-        manager = ModerationObjectsManager()
-        relation_object = generic.GenericRelation(ModeratedObject,
-                                               object_id_field='object_pk')
-        model_class.add_to_class('old_manager', base_manager())
-        model_class.add_to_class(manager_name, manager)
-        model_class.add_to_class('_' + moderated_object_name, relation_object)
+        model_class = moderator_class_instance.model_class
+        base_managers = moderator_class_instance.base_managers 
+        moderation_manager_class\
+         = moderator_class_instance.moderation_manager_class
 
-        def get_modarated_object(self):
-            return getattr(self, '_' + moderated_object_name).get()
-
-        model_class.add_to_class(moderated_object_name,
-                                 property(get_modarated_object))
+        for manager_name, mgr_class in base_managers:
+            ModerationObjectsManager = moderation_manager_class()(mgr_class)
+            manager = ModerationObjectsManager()
+            model_class.add_to_class('unmoderated_%s' % manager_name,
+                                     mgr_class())
+            model_class.add_to_class(manager_name, manager)
+            
+        self._add_moderated_object_to_class(model_class)
 
     def unregister(self, model_class):
         """Unregister model class from moderation"""
         try:
-            moderation_info = self._registered_models.pop(model_class)
+            moderator_instance = self._registered_models.pop(model_class)
         except KeyError:
             msg = "%r has not been registered with Moderation." % model_class
             raise RegistrationError(msg)
 
-        self._remove_fields(model_class, moderation_info)
+        self._remove_fields(moderator_instance)
         self._disconnect_signals(model_class)
 
-    def _remove_fields(self, model_class, moderation_info):
+    def _remove_fields(self, moderator_class_instance):
         """Removes fields from model class and disconnects signals"""
         from django.db.models import signals
-        manager = moderation_info.base_manager()
-
-        model_class.add_to_class(moderation_info.manager_name, manager)
-        delattr(model_class, moderation_info.moderated_object_name)
+        model_class = moderator_class_instance.model_class
+        base_managers = moderator_class_instance.base_managers
+        
+        for manager_name, manager_class in base_managers:
+            manager = manager_class()
+            delattr(model_class, 'unmoderated_%s' % manager_name)
+            model_class.add_to_class(manager_name, manager)
+            
+        delattr(model_class, 'moderated_object')
 
     def _disconnect_signals(self, model_class):
         from django.db.models import signals
@@ -168,13 +235,14 @@ class ModerationManager(object):
         content_object of moderated_object
         """
         pk = instance.pk
-
+        moderator_instance = self._registered_models[sender]
         if kwargs['created']:
             old_object = sender._default_manager.get(pk=pk)
             moderated_object = ModeratedObject(content_object=old_object)
             moderated_object.save()
-            moderated_object.notification.inform_moderator()
+            moderator_instance.inform_moderator(instance)
         else:
+
             moderated_object = ModeratedObject.objects.get(object_pk=pk)
 
             if moderated_object._is_not_equal_instance(instance):
@@ -187,22 +255,13 @@ class ModerationManager(object):
 
                 moderated_object.moderation_status = MODERATION_STATUS_PENDING
                 moderated_object.save()
-                moderated_object.notification.inform_moderator()
-                
+                moderator_instance.inform_moderator(instance)
+
     def _copy_model_instance(self, obj):
         initial = dict([(f.name, getattr(obj, f.name))
                     for f in obj._meta.fields
                     if not f in obj._meta.parents.values()])
         return obj.__class__(**initial)
-    
-    def get_notification_class(self, model_class):
-        try:
-            moderation_info = moderation._registered_models[model_class]
-            ModerationNotification = moderation_info.notification_class
-        except KeyError:
-            ModerationNotification = BaseModerationNotification
-    
-        return ModerationNotification
 
 
 moderation = ModerationManager()
