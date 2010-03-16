6226830ecbbd24c793f5e7aec371f75a7c82ead3
Dominik Szopa <dszopa@gmail.com>
2010-03-16 19:30:34 +0100

    Added test models for register model class with multiple managers

--- src/moderation/tests/test_app/models.py
+++ src/moderation/tests/test_app/models.py
@@ -3,6 +3,7 @@ Test models used in django-moderations tests
 """
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.manager import Manager
 
 
 class UserProfile(models.Model):
@@ -20,3 +21,24 @@ class ModelWithSlugField(models.Model):
 
 class ModelWithSlugField2(models.Model):
     slug = models.SlugField(unique=True)
+
+
+class MenMenager(Manager):
+    
+    def get_query_set(self):
+        query_set = super(MenMenager, self).get_query_set()
+        return query_set.filter(gender=1)
+
+
+class WomenMenager(Manager):
+    
+    def get_query_set(self):
+        query_set = super(WomenMenager, self).get_query_set()
+        return query_set.filter(gender=0)
+
+
+class ModelWithMultipleManagers(models.Model):
+    gender = models.SmallIntegerField()
+    objects = Manager()
+    men = MenMenager()
+    women = WomenMenager()
