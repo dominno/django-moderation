bfe718525f4dcdd0d4573535c242fae65e398017
Dominik Szopa <dszopa@gmail.com>
2010-03-17 19:57:38 +0100

    Added tests for is_auto_reject and is_auto_approve methods

--- src/moderation/tests/test_generic_moderator.py
+++ src/moderation/tests/test_generic_moderator.py
@@ -3,10 +3,12 @@ from moderation.tests.test_app.models import UserProfile
 from moderation import GenericModerator
 from moderation.managers import ModerationObjectsManager
 from django.core import mail
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from moderation.models import ModeratedObject
 from django.db.models.manager import Manager
 import unittest
+from django.test.testcases import TestCase
+from mock import Mock
 
 
 class GenericModeratorTestCase(SettingsTestCase):
@@ -67,6 +69,74 @@ class GenericModeratorTestCase(SettingsTestCase):
         moderator = GenericModerator(UserProfile)
         moderator.inform_user(self.user, self.user)
         self.assertEqual(len(mail.outbox), 1)
+
+
+class AutoModerateTestCase(TestCase):
+    fixtures = ['test_users.json']
+    
+    def setUp(self):
+        self.user = User.objects.get(username='admin')
+        self.moderator = GenericModerator(UserProfile)
+
+    def test_is_auto_approve_user_superuser(self):
+        self.moderator.auto_approve_for_superusers = True
+        self.user.is_superuser = True
+        self.assertTrue(self.moderator.is_auto_approve(self.user))
+
+    def test_is_auto_approve_user_is_staff(self):
+        self.moderator.auto_approve_for_staff = True
+        self.assertTrue(self.moderator.is_auto_approve(self.user))
+
+    def test_is_auto_approve_not_user_superuser(self):
+        self.moderator.auto_approve_for_superusers = True
+        self.moderator.auto_approve_for_staff = True
+        self.user.is_superuser = False
+        self.user.is_staff = False
+        self.assertFalse(self.moderator.is_auto_approve(self.user))
+
+    def test_is_auto_approve_not_user_is_staff(self):
+        self.moderator.auto_approve_for_staff = True
+        self.user.is_staff = False
+        self.user.is_superuser = False
+        self.assertFalse(self.moderator.is_auto_approve(self.user))
+
+    def test_auto_approve_for_groups_user_in_group(self):
+        self.moderator.auto_approve_for_superusers = False
+        self.moderator.auto_approve_for_staff = False
+        self.moderator.auto_approve_for_groups = ['moderators']
+        group = Group(name='moderators')
+        group.save()
+        self.user.groups.add(group)
+        self.user.save()
+        self.assertTrue(self.moderator.is_auto_approve(self.user))
+
+    def test_auto_approve_for_groups_user_not_in_group(self):
+        self.moderator.auto_approve_for_superusers = False
+        self.moderator.auto_approve_for_staff = False
+        self.moderator.auto_approve_for_groups = ['banned']
+        self.assertFalse(self.moderator.is_auto_approve(self.user))
+
+    def test_is_auto_reject_user_is_anonymous(self):
+        self.user.is_anonymous = Mock()
+        self.user.is_anonymous.return_value = True
+        self.assertTrue(self.moderator.is_auto_reject(self.user))
+
+    def test_is_auto_reject_user_is_not_anonymous(self):
+        self.user.is_anonymous = Mock()
+        self.user.is_anonymous.return_value = False
+        self.assertFalse(self.moderator.is_auto_reject(self.user))
+
+    def test_auto_reject_for_groups_user_in_group(self):
+        self.moderator.auto_reject_for_groups = ['banned']
+        group = Group(name='banned')
+        group.save()
+        self.user.groups.add(group)
+        self.user.save()
+        self.assertTrue(self.moderator.is_auto_reject(self.user))
+
+    def test_auto_reject_for_groups_user_not_in_group(self):
+        self.moderator.auto_reject_for_groups = ['banned']
+        self.assertFalse(self.moderator.is_auto_reject(self.user))
 
 
 class BaseManagerTestCase(unittest.TestCase):
