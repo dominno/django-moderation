cc7003804b747d8639b1b2b18586298ed4e117bb
Moustacha <moustacha@gmail.com>
2014-05-17 17:41:20 +0800

    Setup.py test requirements
    If the python version being run is less than 2.7 then install ipython>=0.10,<2 because ipython2 doesn't support < 2.7

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 import os
+import sys
 
 version = '0.3.4'
+
+tests_require = ['django>=1.3.7,<1.6', 'django-webtest>=1.5.7,<1.6',
+                 'webtest>=2.0,<2.1', 'mock', 'pillow', 'unittest2', 'ipdb']
+
+# ipython>2 is only supported on Python 2.7+
+if sys.hexversion < 0x02070000:
+    tests_require = ['ipython>=0.10,<2'] + tests_require
 
 setup(name='django-moderation',
       version=version,
       description="Generic Django objects moderation application",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      
+
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -24,17 +32,9 @@ setup(name='django-moderation',
       url='http://github.com/dominno/django-moderation',
       license='BSD',
       packages = find_packages('.'),
-      
+
       include_package_data=True,
-      tests_require=[
-        'django>=1.3.7,<1.6',
-        'django-webtest>=1.5.7,<1.6',
-        'webtest>=2.0,<2.1',
-        'mock',
-        'pillow',
-        'unittest2',
-        'ipdb'
-      ],
+      tests_require=tests_require,
       test_suite='runtests.runtests',
       install_requires=[
           'django>=1.3.7,<1.6',
