5982406d2247f65cba4f48a8a18461d60cc70ec4
Drew Hubl <drew.hubl@gmail.com>
2017-06-30 15:03:27 -0400

    Make flake8 happy

--- moderation/admin.py
+++ moderation/admin.py
@@ -31,6 +31,7 @@ def approve_objects(modeladmin, request, queryset):
     for obj in queryset:
         obj.approve(by=request.user)
 
+
 approve_objects.short_description = _("Approve selected moderated objects")
 
 
@@ -38,11 +39,13 @@ def reject_objects(modeladmin, request, queryset):
     for obj in queryset:
         obj.reject(by=request.user)
 
+
 reject_objects.short_description = _("Reject selected moderated objects")
 
 
 def set_objects_as_pending(modeladmin, request, queryset):
     queryset.update(status=MODERATION_STATUS_PENDING)
+
 
 set_objects_as_pending.short_description = _("Set selected moderated"
                                              " objects as Pending")
