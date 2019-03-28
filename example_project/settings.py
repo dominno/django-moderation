8f6b10a7b9845f91a33da3558bac99f2c43411f8
Michael Schmid <michael.schmid@rz.uni-augsburg.de>
2019-03-28 15:07:37 +0100

    update de translations

--- example_project/settings.py
+++ example_project/settings.py
@@ -5,6 +5,8 @@ ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
+DEBUG = True
+
 MANAGERS = ADMINS
 
 DATABASES = {
@@ -20,7 +22,22 @@ DATABASES = {
 
 TIME_ZONE = 'America/Chicago'
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join('../moderation/locale'),
+]
+
+LANGUAGES = (
+    ('de', 'German'),
+    ('en', 'English'),
+)
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
