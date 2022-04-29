d46e217087928cf880a7ba4105a6d85dfadeba5d
MaybeThisIsRu <hello@rusingh.com>
2022-04-29 14:35:29 +0530

    Use auto discovery config as default

--- moderation/apps.py
+++ moderation/apps.py
@@ -5,9 +5,12 @@ class SimpleModerationConfig(AppConfig):
     name = 'moderation'
     verbose_name = 'Moderation'
     default_auto_field = 'django.db.models.AutoField'
+    default = False
 
 
 class ModerationConfig(SimpleModerationConfig):
+    default = True
+
     def ready(self):
         from django.utils.module_loading import autodiscover_modules
 
